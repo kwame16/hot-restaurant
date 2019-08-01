609a5aef60cc1080ebe5ccfd6444542590bdbd1e
kwame16 <51427903+kwame16@users.noreply.github.com>
2019-08-01 10:37:52 -0400

    changed js file to new code

--- server.js
+++ server.js
@@ -6,36 +6,39 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3030;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
+// (DATA)
 // =============================================================
-var characters = [
+var reservations = [
   {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
+    routeName: "111",
+    table: 1,
+    customer_name: "John Smith",
+    phone_number: "123-456-7890",
+    email: "john.smith@gmail.com",
+    customer_id: 111
   },
   {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
+    routeName: "222",
+    table: 2,
+    customer_name: "John Smith",
+    phone_number: "123-456-7890",
+    email: "john.smith@gmail.com",
+    customer_id: "222"
   },
   {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
+    routeName: "333",
+    table: 3,
+    customer_name: "John Smith",
+    phone_number: "123-456-7890",
+    email: "john.smith@gmail.com",
+    customer_id: "333"
+  },
 ];
 
 // Routes
@@ -43,27 +46,31 @@ var characters = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
+app.get("/api/tables", function(req, res) {
+  return res.json(reservations);
 });
 
 // Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
+app.get("/api/tables/:custID", function(req, res) {
+  let chosen  = req.params.custID;
 
   console.log(chosen);
 
   for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+    if (chosen === tables[i].routeName) {
+      return res.json(tables[i]);
     }
   }
 
